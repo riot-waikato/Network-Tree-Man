961cad9829727792f8bdb2edfc314dab8d4919ac
Partinarel <partinarel@gmail.com>
2016-06-16 15:36:56 +1200

    Options now less silly, cleaned up the code a bit. IW_select will now actually check through AP's. todo: 	auth 	forward 	re-establish

--- IW_scanfilter.java
+++ IW_scanfilter.java
@@ -17,16 +17,22 @@ public class IW_scanfilter {
 	    String line;
 	    BufferedReader in = new BufferedReader(new InputStreamReader(proc.getInputStream()));
 	    List<String[]> strl = filter_scan(parse_scan(in));
-	    
-	    if(args.length > 0) {
-		if(args[0].equals("-v")) {
-		    print_list(strl);
-		}
+
+	    boolean verbose = false;
+	    boolean connect = false;
+	    for(int i = 0; i < args.length; i++) {
+		if(args[i].equals("-v"))
+		    verbose = true;
+		else if (args[i].equals("-c"))
+		    connect = true;
 	    }
 
-	    if(args.length > 1)
-		if(args[1].equals("-c"))
-		    connect_wpa_cli(strl.get(0), psw);
+	    if(verbose)
+		print_list(strl);
+	    if(connect) {
+		connect_wpa_cli(strl.get(0), psw, verbose);
+		get_server_ip(dev);
+	    }
 	    return strl;
 	}
 	catch(Exception e) {
@@ -35,8 +41,30 @@ public class IW_scanfilter {
 	}
     }
 
-    public static void connect_wpa_cli(String[] pair, String pw) {
+    public static String get_server_ip(String dev) {
+	String[] argv1 = arg_argv("ip route show");
 	
+	List<String> l = null;
+	try { l = (run(argv1));}
+	catch (Exception e){}
+	print_list_2(l);
+	
+	String match = "default via ";
+	for(int i = 0; i < l.size(); i++) {
+	    String s = l.get(i);
+	    if(string_startswith(s, match)) {
+		if(s.contains(dev)) {
+			String target = s.substring(match.length()).split(" ")[0];
+			System.out.println("***"+target);
+			return target;
+		}	    
+	    }
+	}
+	return null;
+    }
+    
+    public static void connect_wpa_cli(String[] pair, String pw, Boolean print) {
+	String preface = "wpa_cli -i " + dev;
 	String[] argv1 = arg_argv("wpa_cli -i " + dev + " disable_network 0");
 	String[] argv2 = arg_argv("wpa_cli -i " + dev + "  remove_network 0");
 	String[] argv3 = arg_argv("wpa_cli -p /var/run/wpa_supplicant -i "+dev+" add_network");
@@ -44,24 +72,56 @@ public class IW_scanfilter {
 	String[] argv5 = arg_argv("wpa_cli -i " + dev + "  set_network 0 psk '\"" + pw + "\"'");
 	String[] argv6 = arg_argv("wpa_cli  -i " + dev + " enable_network 0");
 
-	
+	/*String[] set = {preface + " disable_network 0",
+		                  preface + "remove_network 0",
+		                  "wpa_cli -p /var/run/wpa_supplicant -i " +dev+" add_network",
+		                  preface + "set_network 0 ssid '\"" +pair[1] + "\"'",
+		                  preface + "set_network 0 psk '\"" + pw + "\"'",
+		                  preface + " enable_network 0"
+				  };*/
 	try {
-	    print_list_2(run(argv1));
+	    //print_list_2(run_list(from_argset(set)));
+	    List<List<String>> outset = new LinkedList<List<String>>();
+	    outset.add(run(argv1));
+	    outset.add(run(argv2));
+	    outset.add(run(argv3));
+	    outset.add(run(argv4));
+	    outset.add(run(argv5));
+	    outset.add(run(argv6));
+
+	    if(print)
+		for(int i = 0; i < 6; i++)
+		    print_list_2(outset.get(i));
+	    /*print_list_2(run(argv1));
 	    print_list_2(run(argv2));
 	    print_list_2(run(argv3));
 	    print_list_2(run(argv4));
 	    print_list_2(run(argv5));
-	    print_list_2(run(argv6));
+	    print_list_2(run(argv6));*/
 	}
 	catch (Exception e) {
 	    System.out.println(e);
 	}
     }
 
+    static String[][] from_argset(String[] cin) {
+	String[][] cout = new String[cin.length][];
+	for(int i = 0; i < cin.length; i++)
+	    cout[i] = arg_argv(cin[i]);
+	return cout;
+    }
+
+    static List<String> run_list(String[][] argv) throws Exception {
+	List<String> l = new LinkedList<String>();
+	for(int i = 0; i < argv.length; i++){
+	    l.addAll(run(argv[i]));
+	}
+	return l;
+    }
     static String[] arg_argv(String arg) {
 	return new String[] {"/bin/bash", "-c", arg};
     }
-    public static void connect_iw(String[] pair, String pw, String fn) {
+    /*public static void connect_iw(String[] pair, String pw, String fn) {
 	String[] argv  = {"/bin/bash", "-c", "rm " + fn};
 	String[] argv3 = {"/bin/bash","-c","wpa_supplicant -B -D nl80211 -i" + dev + " -c" + fn};
 	try {
@@ -73,9 +133,9 @@ public class IW_scanfilter {
 	catch(Exception e) {
 	    System.out.println(e);
 	}
-    }
+	}*/
 
-    private static void wpa_passphrase_adj(String[] pair, String pw, String fn) throws Exception {
+    /*private static void wpa_passphrase_adj(String[] pair, String pw, String fn) throws Exception {
 	List<String> str = run(new String[] {"/bin/bash", "-c", "wpa_passphrase " + pair[1] + " " + pw});
 	String rep1  =str.get(2).split("#")[0] + str.get(2).split("#")[1];
 	String rep2  ="#" + str.get(3);
@@ -86,7 +146,7 @@ public class IW_scanfilter {
 
 	for(int i = 0; i < str.size(); i++)
 	    dunk_into_file(str.get(i), fn);
-    }
+	    }*/
 
     private static void dunk_into_file (String str, String fn) throws Exception {
 	run(new String[] {"/bin/bash", "-c", "echo '" + str + "' >> " + fn});
--- IW_select.java
+++ IW_select.java
@@ -16,18 +16,22 @@ public class IW_select {
 	    return;
 
 	//test
-	IW_scanfilter.connect_wpa_cli(pairs.get(0), pw);
+	//IW_scanfilter.connect_wpa_cli(pairs.get(0), pw, true);
 	//end
 	
 	/* check if we're already connected to our target device before doing this */
 	
-	//for(int i = 0; i < pairs.size(); i++) {
+	for(int i = 0; i < pairs.size(); i++) {
+	    if(pairs.get(i)[1].charAt(pairs.get(i)[1].length()-1) != 'A') {
+		System.out.println("Skipping " + pairs.get(i)[1]);
+		continue;
+	    }
 	    /* connect to the AP */
-	//  IW_scanfilter.connect_iw(pairs.get(i), IW_scanfilter.psw, IW_scanfilter.fname);
+	    IW_scanfilter.connect_wpa_cli(pairs.get(i), IW_scanfilter.psw, true);
 	    /* then check if this AP has a connection to the target device */
-	//    if(true)
+	    //if(true)
 		/*if it does, then we exit this loop - we should have a fixed connection state */
-	//	break;
-	//}
+		//break;
+	}
     }
 }
